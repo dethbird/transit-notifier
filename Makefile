f0af2008eb54a1ef7c059f83d28e6274ad4ea54f
transit <rishi.satsangi@gmail.com>
2018-11-23 11:35:21 -0500

    add composer dependencies

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ env: requirements.txt
 	pip3 install -r requirements.txt
 	yarn install
 	python3 src/python/init.py
+	curl -sS https://getcomposer.org/installer | php
+	php composer.phar install
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -4,19 +4,18 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
-import { Card } from 'primereact/card';
 
 import Settings from 'components/pages/settings';
 import Status from 'components/pages/status';
 
 const App = () => (
-  <Router>
-      <Switch>
-        <Route exact path="/" component={ Status } />
-        <Route path="/status" component={ Status } />
-        <Route path="/settings" component={ Settings } />
-      </Switch>
-  </Router>
+    <Router>
+        <Switch>
+            <Route exact path="/" component={ Status } />
+            <Route path="/status" component={ Status } />
+            <Route path="/settings" component={ Settings } />
+        </Switch>
+    </Router>
 );
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
