9005dc73eab1b620105c6610dfab0ae9f9126357
transit <rishi.satsangi@gmail.com>
2018-11-24 06:44:38 -0500

    main menu is working

--- src/frontend/components/layout/external-layout.js
+++ src/frontend/components/layout/external-layout.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Link } from "react-router-dom";
+import { withRouter  } from "react-router-dom";
+import { Container, Menu } from 'semantic-ui-react'
 
 class ExternalLayout extends React.Component {
     render() {
+        const { history, match } = this.props;
         return (
             <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/status">Status</Link>
-                        </li>
-                        <li>
-                            <Link to="/settings">Settings</Link>
-                        </li>
-                    </ul>
-                </nav>
-                {this.props.children}
+                <br />
+                <Container>
+                    <Menu tabular>
+                        <Menu.Item name='Status' active={ ['/', 'status'].indexOf(match.path) >= 0 } onClick={()=>{ history.push('/') }} />
+                        <Menu.Item name='Settings' active={ match.path=='/settings' } onClick={()=>{ history.push('/settings') }} />
+                    </Menu>
+                </Container>
+                <Container>
+                    {this.props.children}
+                </Container>
             </div>);
     }
 }
 
-export default ExternalLayout;
+export default withRouter(ExternalLayout);
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, browserHistory } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import 'primereact/resources/themes/nova-light/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import 'semantic-ui-css/semantic.min.css';
 
 import store from "store/store";
 import Settings from 'components/pages/settings';
@@ -13,11 +11,11 @@ import Status from 'components/pages/status';
 
 const App = () => (
     <Provider store={store}>
-        <Router>
+        <Router history={ browserHistory }>
             <Switch>
-                <Route exact path="/" component={Status} />
-                <Route path="/status" component={Status} />
-                <Route path="/settings" component={Settings} />
+                <Route exact path="/" component={ Status } />
+                <Route path="/status" component={ Status } />
+                <Route path="/settings" component={ Settings } />
             </Switch>
         </Router>
     </Provider>
